e1d0b7c7f364d6fd4826127ee8c17dacbf8e03ba
Biolab Helper <biolab-helper@staric.net>
2017-02-14 13:18:45 +0100

    Implement data fetching

--- gen_info.py
+++ gen_info.py
@@ -1,18 +1,33 @@
 from collections import OrderedDict
 from glob import glob
 import json
-import os.path
-from sys import argv
+from os import chdir, getcwd
+from os.path import isfile, join, normpath
+from sys import argv, stderr
+from urllib.request import urlretrieve
 
 
-start_dir = os.getcwd()
+start_dir = getcwd()
 info = []
 for root in argv[1:]:
-    os.chdir(os.path.join(start_dir, root))
+    chdir(join(start_dir, root))
     for infof in glob('**/*.info', recursive=True):
+        changed = False
         with open(infof, 'r') as f:
             d = json.load(f, object_pairs_hook=OrderedDict)
-        info.append([[root, infof[:-5]], d])
+            filename = infof[:-5]
+            location = d.get('file', '')
+            if not isfile(filename) and location:
+                try:
+                    urlretrieve(location, filename)
+                    d['file'] = 'http://butler.fri.uni-lj.si/datasets/{}/{}'.format(normpath(root), filename)
+                    changed = True
+                except:
+                    print('failed to get file', filename, location, file=stderr)
+        if changed:
+            with open(infof, 'w') as f:
+                json.dump(d, f, indent=4)
+        info.append([[root, filename], d])
 
 info.sort()
 print(json.dumps(info))
