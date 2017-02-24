29e854f3b7644293a76ec157f9b6df58f4c79d7c
Lan Zagar <lan.zagar@fri.uni-lj.si>
2017-02-24 16:35:41 +0100

    Update lint.py

--- utils/lint.py
+++ utils/lint.py
@@ -1,19 +1,31 @@
 from collections import OrderedDict
 from glob import glob
 import json
-from os import chdir, getcwd
 from os.path import basename, splitext
-from sys import argv, stderr
-from urllib.request import urlretrieve, urlopen
+from sys import exit
+from urllib.request import urlopen
 
 
+MANDATORY = ('name', 'description', 'version',
+             'instances', 'variables', 'target')
+
+
+ret = 0
+print('Testing:')
 for infof in glob('**/*.info', recursive=True):
-    print(infof)
+    print(infof, end=' ... ')
     with open(infof, 'r') as f:
         d = json.load(f, object_pairs_hook=OrderedDict)
         filename = splitext(basename(infof)[:-5])[0]
-        assert filename == d['name']
+        assert filename == d['name'], 'name field does not match the filename'
         location = d['file']
-        remotefile = urlopen(location)
-        for key in ('name', 'description', 'instances', 'variables', 'version'):
-            assert key in d
+        try:
+            remotefile = urlopen(location)
+        except:
+            print('Cannot open remote file')
+            ret = 1
+        for key in MANDATORY:
+            assert key in d, 'Missing field: {}'.format(key)
+    print('OK')
+
+exit(ret)
