fd151baccee2b369303f9aa295569bcdfb009bd2
Lan Zagar <lan.zagar@fri.uni-lj.si>
2017-02-24 15:35:06 +0100

    Add checking of .info files

--- utils/lint.py
+++ utils/lint.py
@@ -2,36 +2,18 @@ from collections import OrderedDict
 from glob import glob
 import json
 from os import chdir, getcwd
-from os.path import getsize, isfile, join, normpath
+from os.path import basename, splitext
 from sys import argv, stderr
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve, urlopen
 
 
-URL = 'http://butler.fri.uni-lj.si/datasets'
-
-
-start_dir = getcwd()
-info = []
-for root in argv[1:]:
-    chdir(join(start_dir, root))
-    for infof in glob('**/*.info', recursive=True):
-        changed = False
-        with open(infof, 'r') as f:
-            d = json.load(f, object_pairs_hook=OrderedDict)
-            filename = infof[:-5]
-            location = d.get('file', '')
-            if location and not location.startswith(URL):
-                try:
-                    urlretrieve(location, filename)
-                    d['file'] = '{}/{}/{}'.format(URL, normpath(root), filename)
-                    d['size'] = getsize(filename)
-                    changed = True
-                except:
-                    print('failed to get file', filename, location, file=stderr)
-        if changed:
-            with open(infof, 'w') as f:
-                json.dump(d, f, indent=4)
-        info.append([[root, filename], d])
-
-info.sort()
-print(json.dumps(info, indent=4))
+for infof in glob('**/*.info', recursive=True):
+    print(infof)
+    with open(infof, 'r') as f:
+        d = json.load(f, object_pairs_hook=OrderedDict)
+        filename = splitext(basename(infof)[:-5])[0]
+        assert filename == d['name']
+        location = d['file']
+        remotefile = urlopen(location)
+        for key in ('name', 'description', 'instances', 'variables', 'version'):
+            assert key in d
